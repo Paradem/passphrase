0a73f65af18a7db2cbe8db556f6b194b41b854db
edbedbe <eli@edbed.be>
2018-06-29 02:55:37 -0400

    added package info for publishing to hex

--- mix.exs
+++ mix.exs
@@ -2,19 +2,33 @@ defmodule Passphrase.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :passphrase,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :passphrase,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      deps: deps(),
+      description: """
+      Passphrase generates secure, random, easy-to-remember
+      passphrases consisting of space-separated simple English words.
+      """,
+      package: package(),
+      name: "Passphrase",
+      source_url: "https://github.com/edbedbe/passphrase/",
+    ]
   end
 
-  def application do
-    [extra_applications: [:logger]]
+  def package do
+    [
+      name: :passphrase,
+      licenses: ["MIT"],
+      maintainers: ["Eli Bierman"],
+      links: %{"Source Code" => "https://github.com/edbedbe/passphrase/"},
+    ]
   end
 
-  defp deps do
-    []
+  def deps do
+    [
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+    ]
   end
 end

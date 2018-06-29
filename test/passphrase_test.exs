22f889ece6d0407ed03d65bc7bad82fc47b3fe70
edbedbe <eli@edbed.be>
2018-06-29 02:26:21 -0400

    added basic test

--- test/passphrase_test.exs
+++ test/passphrase_test.exs
@@ -2,9 +2,10 @@ defmodule PassphraseTest do
   use ExUnit.Case
   doctest Passphrase
 
-  test "the truth" do
-    p = Passphrase.new()
-    len = String.length(p)
-    assert len > 9 && len < 29
+  test "passphrase is right amount of words" do
+    p = Passphrase.new(5)
+    assert length(String.split(p, " ")) == 5
+    p = Passphrase.new(3)
+    assert length(String.split(p, " ")) == 3
   end
 end
